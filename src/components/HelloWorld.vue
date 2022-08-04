31c381442982e28a2b5593f7007857f134aa3653
aathym <aat8001@gmail.com>
2022-08-04 23:11:46 +0530

    computed in composition api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,22 +2,59 @@
   <div class="hello">
     <h1>{{ age }}</h1>
 
+
+
+
+    <!-- <h2>{{firstName}}</h2>
+    <h2>{{lastName}}</h2> -->
+
+
+    {{fullName}}
+
     <button @click="setNewData">change age</button>
+
+
+
+
+    <div class="">
+      <input type="text" placeholder="firstname" @input="setFirstName" />
+      <input type="text" placeholder="lastname" @input="setLastName" />
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+
+// computed is a function
 
 const age = ref(12);
+const firstName = ref("");
+
+const lastName = ref("");
 
 
 
 
+// fullName is a computed property
+// fullName is also a ref , computed values are also reactive but only read only
+const fullName =  computed(function(){
+  return firstName.value + ' ' + lastName.value
+})
 
-function setNewData(){
-     age.value=45
+function setNewData() {
+  age.value = 45;
 }
+
+function setFirstName(event) {
+  firstName.value = event.target.value;
+}
+
+function setLastName(event) {
+  lastName.value = event.target.value;
+}
+
+
 
 </script>
 
